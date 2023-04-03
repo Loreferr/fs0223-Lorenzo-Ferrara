7acb171c930a71ecf004bdf2c85562b762781692
Loreferr <lf.95@outlook.com>
2023-04-03 16:51:08 +0200

    Esercizio G1

---
+++ m1/s2/G1/js/script.js
@@ -0,0 +1,48 @@
+document.getElementById('prova').innerHTML = '<b>AncoraggioID</b>'
+
+
+var città = 'Cava'
+var numero = 35
+console.log(numero);
+document.write(numero)
+
+console.log(città + 'ha' + numero + 'abitanti');
+
+numero = 60000
+
+console.log(città + 'ha' + numero + 'abitanti');
+
+
+var numero = 30
+
+if (numero == 30){
+    console.log('Giusto')
+}
+else if(numero > 30){
+    console.log('è troppo')
+}
+else{
+    console.log('poco')
+}
+
+console.log( città + 'ha' + numero + 'abitanti')
+
+console.log( città + ' ha ' + numero*5000 + ' abitanti ')
+
+var giorni = 30
+var mesi = 12
+
+document.write(numero + mesi)
+
+function testo(){
+    document.write('Sotto la Panca la capra Campa')
+    document.write('Sotto la Panca la capra Campa')
+    document.write('Sotto la Panca la capra Campa')
+    document.write('Sotto la Panca la capra Campa')
+    document.write('Sotto la panca la capra crepa')
+}
+
+testo()
+testo()
+
+document.write('4'* mesi* giorni)
---
+++ m1/s2/G1/js/script2.js
@@ -0,0 +1,16 @@
+var scudettiJuve = prompt ('Quanti scudetti ha la juve?')
+
+console.log(scudettiJuve);
+
+
+if(scudettiJuve == 36){  
+document.write('true')}
+else if(scudettiJuve > 36){
+document.write('false')
+location.href = 'https://i.guim.co.uk/img/media/e000688771b673aaebce51593cec6239ba54c5b7/0_214_5007_3005/master/5007.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=8ef293a7aa370f694a6bf68e0711f4d0';
+}
+else if(scudettiJuve <36){
+    document.write('false')
+}
+
+
