2ec5f1984ae846fa10c2cd15edaec52239a990c4
Loreferr <lf.95@outlook.com>
2023-06-21 04:16:26 +0200

    Capstone

---
+++ Capstone/WhereITravel/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomepageComponent } from './Pages/homepage/homepage.component';
+import { HeaderComponent } from './Pages/header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
